5f8d57760ec4a8473f9e8eefc05d2b213f9844ec
Michel M <Michel@dynamic.wline.6rd.res.cust.swisscom.ch>
2017-12-14 00:55:30 +0100

    Initial Commit

---
+++ MakeItFamous/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakeItFamous
+//
+//  Created by Michel M. on 14/12/17.
+//  Copyright © 2017 mmarki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
