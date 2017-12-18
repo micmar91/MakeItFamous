0d993e153cee85e477cf75b129cccbcedb9a649f
Michel M <Michel@Students-MacBook-Air-3.local>
2017-12-18 19:33:53 +0100

    sign in ui custom classes completed

---
+++ MakeItFamous/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MakeItFamous
+//
+//  Created by Michel M. on 18/12/17.
+//  Copyright © 2017 mmarki. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
---
+++ MakeItFamous/FancyField.swift
@@ -0,0 +1,30 @@
+//
+//  FancyField.swift
+//  MakeItFamous
+//
+//  Created by Michel M. on 18/12/17.
+//  Copyright © 2017 mmarki. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 5.0
+        
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10.0, dy: 5.0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10.0, dy: 5.0)
+        
+    }
+}
